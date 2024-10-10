1b41a47838b5f199eb639f7e549578e21f2f76f9
aldo xavier <aldoxaviermlg@gmail.com>
2024-10-10 21:49:02 +0700

    sign in dan sign up

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/signin.css">
+    <title>Sign in </title>
+</head>
+<body>
+    <div class="semua">
+        <div class="kiri">
+            <div class="top">
+                <div class="logoo"><img src="/image/Blue.png" class="logo"></div>
+                <form class="form">
+                    <div class="container">
+                        <h1 class="">Sign In</h1>
+                        <h3 class="">please enter your details below</h4>
+                        <div class="fields">
+                            <label for="email" >Email :</label><br>
+                            <input type="text" id="email" placeholder="Email">
+                        </div>
+                        <div class="fields">
+                            <label for="password" >Password :</label><br>
+                            <input type="text" id="password" placeholder="Password">
+                        </div>
+                        <p class="fp" >Forgot password?</p>
+                        <div class="fields"><button class="signinbtn">Sign in</button></div>
+                        <p class="createacc">Don't have an account? <a href="">Create Account</a></p>
+                    </div>
+                    
+                    </form>
+            </div>
+        </div>
+        <div class="kanan">
+                <div class="contentkanan">
+                    <h1>Kerja Setara</h1>
+                    <img src="/image/rrrrr.jpeg" alt=""> 
+                </div>
+                     
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/signup.css">
+    <title>Sign up</title>
+</head>
+<body>
+    <div class="semua">
+        <div class="kiri">
+            <div class="top">
+                <div class="logoo"><img src="/image/Blue.png" class="logo"></div>
+                <form class="form">
+                    <div class="container">
+                        <h1 class="">Sign Up</h1>
+                        <h3 class="">please enter your details below</h4>
+                        <div class="fullname">
+                            <div class="fieldsname">
+                                <label for="fname" >First name :</label><br>
+                                <input type="text" id="fname" placeholder="First name">
+                            </div>
+                            <div class="fieldsname">
+                                <label for="fname" >Last name :</label><br>
+                                <input type="text" id="lname" placeholder="Last name">
+                            </div>
+                        </div>
+                        <div class="fields">
+                            <label for="email" >Email :</label><br>
+                            <input type="text" id="email" placeholder="Email">
+                        </div>
+                        <div class="fields">
+                            <label for="password" >Password :</label><br>
+                            <input type="text" id="password" placeholder="Password">
+                        </div>
+                        <div class="fields"><button class="signupbtn">Sign up</button></div>
+                    </div>
+                    
+                    </form>
+            </div>
+        </div>
+        <div class="kanan">
+                <div class="contentkanan">
+                    <h1>Kerja Setara</h1>
+                    <img src="/image/rrrrr.jpeg" alt=""> 
+                </div>
+                     
+        </div>
+    </div>    
+</body>
+</html>
